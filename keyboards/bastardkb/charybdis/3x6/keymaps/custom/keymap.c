f50075b4f743a21f2d57e9289c2398560f1d8523
Gustavo Robles <gustavo.robles@reverb.com>
2025-10-22 16:08:54 -0600

    typi

--- keyboards/bastardkb/charybdis/3x6/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/custom/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT, KC_TRNS, KC_LBRC,     KC_UP,          KC_RBRC,        KC_MNXT,                                KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS,        KC_TRNS, QK_BOOT,
     KC_TRNS, KC_TRNS, KC_LEFT,     KC_DOWN,        KC_RIGHT,       KC_MPLY,                                KC_TRNS, KC_RIGHT_GUI, KC_TRNS, KC_RIGHT_SHIFT, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_VOLD,     KC_MUTE,        KC_VOLU,        KC_MPRV,                                KC_TRNS, KC_RIGHT_ALT, KC_TRNS, KC_RIGHT_CTRL,  KC_TRNS, KC_TRNS,
-                                   KC_TRNS,        KC_TRNS,        KC_GRAVE                                KC_TRNS, KC_TRNS
+                                   KC_TRNS,        KC_TRNS,        KC_GRAVE,                               KC_TRNS, KC_TRNS
   ),
   [LAYER_POINTER] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
